d7be4bdd4d4a976af86e21d1106b9d6142f8a562
Nutti Saelor <leelorz6@gmail.com>
2016-11-13 00:29:56 +0700

    change resource files

--- app/src/main/java/com/leelorz/helloworld/MainActivity.java
+++ app/src/main/java/com/leelorz/helloworld/MainActivity.java
@@ -1,20 +1,129 @@
 package com.leelorz.helloworld;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvHello;
+    EditText editTextHello;
+    Button btnCopy;
+
+    EditText editText1;
+    EditText editText2;
+    TextView tvResult;
+    Button btnCalculate;
+    RadioGroup rgOperator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tvHello = (TextView) findViewById(R.id.tvHello);
+        initInstances();
+    }
+
+    private void initInstances() {
+        tvHello = (TextView) findViewById(R.id.tvHello);
         tvHello.setMovementMethod(LinkMovementMethod.getInstance());
         tvHello.setText(Html.fromHtml("<b>Hello</b> <i>World</i> <font color=\"#fafafa\">La La La</font> <a href=\"http://nuuneoi.com\">nuuneoi.com</a>"));
+
+        editTextHello = (EditText) findViewById(R.id.editTextHello);
+        editTextHello.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    // Copy text in
+                    tvHello.setText(editTextHello.getText());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        btnCopy = (Button) findViewById(R.id.btnCopy);
+        btnCopy.setOnClickListener(this);
+
+        editText1 = (EditText) findViewById(R.id.editText1);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        btnCalculate = (Button) findViewById(R.id.btnCalculate);
+        btnCalculate.setOnClickListener(this);
+
+        rgOperator = (RadioGroup) findViewById(R.id.rgOperator);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnCopy) {
+            tvHello.setText(editTextHello.getText());
+        } else if (v == btnCalculate) {
+            int val1 = 0;
+            int val2 = 0;
+            int result = 0;
+            try {
+                val1 = Integer.parseInt(editText1.getText().toString());
+            } catch (NumberFormatException e) {
+            }
+            try {
+                val2 = Integer.parseInt(editText2.getText().toString());
+            } catch (NumberFormatException e) {
+            }
+
+            switch (rgOperator.getCheckedRadioButtonId()) {
+                case R.id.rbPlus:
+                    result = val1 + val2;
+                    break;
+                case R.id.rbMinus:
+                    result = val1 - val2;
+                    break;
+                case R.id.rbMultiply:
+                    result = val1 * val2;
+                    break;
+                case R.id.rbDivide:
+                    result = val1 / val2;
+                    break;
+            }
+
+            // show result
+            tvResult.setText("= " + result);
+
+            Log.d("Calculation", "Result = " + result);
+
+            Toast.makeText(MainActivity.this,
+                    "Result = " + result,
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            Toast.makeText(MainActivity.this, "Press Button", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
